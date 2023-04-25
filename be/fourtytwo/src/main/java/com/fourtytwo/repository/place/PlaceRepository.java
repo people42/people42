a90442ec07575d96c5cf901d72502b3a35a90733
임희상 <hslim9400@gmail.com>
2023-04-25 15:45:25 +0900

    [FEAT] 사람, 장소 피드 조회 api

--- be/fourtytwo/src/main/java/com/fourtytwo/repository/place/PlaceRepository.java
+++ be/fourtytwo/src/main/java/com/fourtytwo/repository/place/PlaceRepository.java
@@ -4,4 +4,5 @@ import com.fourtytwo.entity.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlaceRepository extends JpaRepository<Place, Long> {
+    Place findPlaceById(Long placeIdx);
 }
