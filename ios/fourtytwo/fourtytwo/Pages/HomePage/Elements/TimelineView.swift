ed63a882b5e388150573b19d7f2f0cc8fb1fde02
qorzi00 <qorzi00@gmail.com>
2023-05-03 02:13:16 +0900

    [FIX] 최신 피드 라인 개선

--- ios/fourtytwo/fourtytwo/Pages/HomePage/Elements/TimelineView.swift
+++ ios/fourtytwo/fourtytwo/Pages/HomePage/Elements/TimelineView.swift
@@ -74,109 +74,121 @@ struct TimelineView: View {
 
     var body: some View {
         if #available(iOS 15.0, *) {
-            ScrollView {
-                VStack(alignment: .leading, spacing: 16) {
-                    if viewModel.messageInfoList.count < 1 {
-                        Spacer()
-                        HStack {
+            ZStack {
+                HStack {
+                    Rectangle()
+                        .fill(Color.gray.opacity(0.3))
+                        .frame(width: 2, height: UIScreen.main.bounds.height)
+                        .padding(.horizontal, 22.5)
+                    Spacer()
+                }
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 16) {
+                        if viewModel.messageInfoList.count < 1 {
                             Spacer()
-                            Text("아직 인연이 없어요")
-                            Spacer()
-                        }
-                        Spacer()
-                    } else {
-                        ForEach(viewModel.messageInfoList.indices, id: \.self) { index in
-                            HStack(alignment: .center, spacing: 8) {
-                                ZStack(alignment: .center) {
-                                    if index < viewModel.messageInfoList.count - 1 {
-                                        DashedLine()
-                                    }
-                                    TimelinePoint()
-                                }
-                                .frame(width: 16)
-                                // 누르면 PlaceView로 이동
-                                MessageCard(messageInfo: viewModel.messageInfoList[index])
-                                    .onTapGesture {
-                                        placeViewState.selectedPlaceID = viewModel.messageInfoList[index].placeIdx
-                                        placeViewState.navigateToPlaceView = true
-                                        placeViewState.placeDate = viewModel.messageInfoList[index].hour
-                                     }
+                            HStack {
+                                Spacer()
+                                Text("아직 인연이 없어요")
+                                Spacer()
                             }
-                            .padding(.bottom, 16)
+                            Spacer()
+                        } else {
+                            ForEach(viewModel.messageInfoList.indices, id: \.self) { index in
+                                HStack(alignment: .center, spacing: 8) {
+                                    ZStack(alignment: .center) {
+                                        TimelinePoint()
+                                    }
+                                    .frame(width: 16)
+                                    // 누르면 PlaceView로 이동
+                                    MessageCard(messageInfo: viewModel.messageInfoList[index])
+                                        .onTapGesture {
+                                            placeViewState.selectedPlaceID = viewModel.messageInfoList[index].placeIdx
+                                            placeViewState.navigateToPlaceView = true
+                                            placeViewState.placeDate = viewModel.messageInfoList[index].hour
+                                         }
+                                }
+                                .padding(.bottom, 16)
+                            }
+                        }
+                    }
+                    .padding()
+                    .padding(.top, 16)
+                }
+                .refreshable {
+                    withAnimation {
+                        refreshing = true
+                        viewModel.fetchRecentFeed()
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                            refreshing = false
                         }
                     }
                 }
-                .padding()
-                .padding(.top, 16)
-            }
-            .refreshable {
-                withAnimation {
-                    refreshing = true
+                .onAppear {
                     viewModel.fetchRecentFeed()
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                        refreshing = false
+                }
+                .onChange(of: scenePhase) { newScenePhase in
+                    if newScenePhase == .active {
+                        // foreground로 전환될 때 데이터를 새로 고칩니다.
+                        viewModel.fetchRecentFeed()
                     }
                 }
-            }
-            .onAppear {
-                viewModel.fetchRecentFeed()
-            }
-            .onChange(of: scenePhase) { newScenePhase in
-                if newScenePhase == .active {
-                    // foreground로 전환될 때 데이터를 새로 고칩니다.
+                .onChange(of: reactionState.reaction) { newValue in
                     viewModel.fetchRecentFeed()
                 }
-            }
-            .onChange(of: reactionState.reaction) { newValue in
-                viewModel.fetchRecentFeed()
             }
         } else {
-            ScrollView {
-                VStack(alignment: .leading, spacing: 16) {
-                    if viewModel.messageInfoList.count < 1 {
-                        Spacer()
-                        HStack {
+            ZStack {
+                HStack {
+                    Rectangle()
+                        .fill(Color.gray.opacity(0.3))
+                        .frame(width: 2, height: UIScreen.main.bounds.height)
+                        .padding(.horizontal, 22.5)
+                    Spacer()
+                }
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 16) {
+                        if viewModel.messageInfoList.count < 1 {
                             Spacer()
-                            Text("아직 인연이 없어요")
-                            Spacer()
-                        }
-                        Spacer()
-                    } else {
-                        ForEach(viewModel.messageInfoList.indices, id: \.self) { index in
-                            HStack(alignment: .center, spacing: 8) {
-                                ZStack(alignment: .center) {
-                                    if index < viewModel.messageInfoList.count - 1 {
-                                        DashedLine()
-                                    }
-                                    TimelinePoint()
-                                }
-                                .frame(width: 16)
-                                // 누르면 PlaceView로 이동
-                                MessageCard(messageInfo: viewModel.messageInfoList[index])
-                                    .onTapGesture {
-                                        placeViewState.selectedPlaceID = viewModel.messageInfoList[index].placeIdx
-                                        placeViewState.navigateToPlaceView = true
-                                        placeViewState.placeDate = viewModel.messageInfoList[index].hour
-                                     }
+                            HStack {
+                                Spacer()
+                                Text("아직 인연이 없어요")
+                                Spacer()
                             }
-                            .padding(.bottom, 16)
+                            Spacer()
+                        } else {
+                            ForEach(viewModel.messageInfoList.indices, id: \.self) { index in
+                                HStack(alignment: .center, spacing: 8) {
+                                    ZStack(alignment: .center) {
+                                        TimelinePoint()
+                                    }
+                                    .frame(width: 16)
+                                    // 누르면 PlaceView로 이동
+                                    MessageCard(messageInfo: viewModel.messageInfoList[index])
+                                        .onTapGesture {
+                                            placeViewState.selectedPlaceID = viewModel.messageInfoList[index].placeIdx
+                                            placeViewState.navigateToPlaceView = true
+                                            placeViewState.placeDate = viewModel.messageInfoList[index].hour
+                                         }
+                                }
+                                .padding(.bottom, 16)
+                            }
                         }
                     }
+                    .padding()
+                    .padding(.top, 16)
                 }
-                .padding()
-                .padding(.top, 16)
-            }
-            .onAppear {
-                viewModel.fetchRecentFeed()
-            }
-            .onChange(of: scenePhase) { newScenePhase in
-                if newScenePhase == .active {
-                    // foreground로 전환될 때 데이터를 새로 고칩니다.
+                .onAppear {
                     viewModel.fetchRecentFeed()
                 }
-            }
-            .onChange(of: reactionState.reaction) { newValue in
-                viewModel.fetchRecentFeed()
+                .onChange(of: scenePhase) { newScenePhase in
+                    if newScenePhase == .active {
+                        // foreground로 전환될 때 데이터를 새로 고칩니다.
+                        viewModel.fetchRecentFeed()
+                    }
+                }
+                .onChange(of: reactionState.reaction) { newValue in
+                    viewModel.fetchRecentFeed()
+                }
             }
         }
     }
@@ -185,20 +197,8 @@ struct TimelineView: View {
 struct TimelinePoint: View {
     var body: some View {
         Circle()
-            .fill(Color.gray.opacity(0.7))
+            .fill(Color.gray)
             .frame(width: 16, height: 16)
-    }
-}
-
-struct DashedLine: View {
-    var body: some View {
-        Path { path in
-            path.move(to: CGPoint(x: 8, y: 92))
-            path.addLine(to: CGPoint(x: 8, y: 292))
-        }
-        .stroke(style: StrokeStyle(lineWidth: 5, dash: [0.1]))
-        .foregroundColor(Color.gray.opacity(0.3))
-        .frame(maxWidth: .infinity, alignment: .leading)
     }
 }
 
