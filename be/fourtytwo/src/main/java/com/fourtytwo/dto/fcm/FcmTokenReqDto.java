e8c13beaedaa6c73cbc1f8c3e2be09efbfb1a223
윤성운 <ysu6691@gmail.com>
2023-05-08 11:03:28 +0900

    [FIX] FCM token 유효성검사 추가

--- be/fourtytwo/src/main/java/com/fourtytwo/dto/fcm/FcmTokenReqDto.java
+++ be/fourtytwo/src/main/java/com/fourtytwo/dto/fcm/FcmTokenReqDto.java
@@ -4,11 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class FcmTokenReqDto {
 
+    @NotBlank
     private String token;
 
 }
