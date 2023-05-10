12478dc6311c8d0618a8e3dc29de0ebb8c177f28
임희상 <hslim9400@gmail.com>
2023-05-10 17:03:45 +0900

    [FIX] 메세지 쿼리 수정

--- be/fourtytwo/src/main/java/com/fourtytwo/repository/message/MessageRepository.java
+++ be/fourtytwo/src/main/java/com/fourtytwo/repository/message/MessageRepository.java
@@ -6,6 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MessageRepository extends JpaRepository<Message, Long>, MessageRepositoryCustom {
 
-    Long countByIsActiveIsTrueAndIsInappropriateIsFalse();
-
 }
