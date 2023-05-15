e9cb62e81ceec0b763c713d9a0fcd9a68fd0b4b5
정민우 <minu.j.dev@gmail.com>
2023-05-15 15:23:18 +0900

    [FEAT] apple smart app banner

--- fe/forty-two/src/Meta.tsx
+++ fe/forty-two/src/Meta.tsx
@@ -6,20 +6,22 @@ type metaProps = {
 };
 
 function Meta(props: metaProps) {
+  const IOS_URL = import.meta.env.VITE_IOS_URL;
   return (
     <Helmet>
       {/* Primary Meta Tags */}
       <title>{props.title}</title>
       <meta name="title" content={props.title} />
 
-      {/* iOS
-      <meta property="al:ios:url" content=" ios 앱 URL" />
-      <meta property="al:ios:app_store_id" content="ios 앱스토어 ID" />
-      <meta property="al:ios:app_name" content="ios 앱 이름" />
-      Android
-      <meta property="al:android:url" content="안드로이드 앱 URL" />
-      <meta property="al:android:app_name" content="안드로이드 앱 이름" />
-      <meta property="al:android:package" content="안드로이드 패키지 이름" />
+      {/* iOS */}
+      <meta property="al:ios:url" content={IOS_URL} />
+      <meta property="al:ios:app_store_id" content="6448700604" />
+      <meta property="al:ios:app_name" content="사이" />
+      <meta name="apple-itunes-app" content="app-id=6448700604" />
+      {/* Android */}
+      <meta property="al:android:app_name" content="42" />
+      <meta property="al:android:package" content="com.cider.fourtytwo" />
+      {/* <meta property="al:android:url" content="안드로이드 앱 URL" />
       <meta property="al:web:url" content="안드로이드 앱 URL" /> */}
     </Helmet>
   );
